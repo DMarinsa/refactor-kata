8bcaf1655396a9d35828823c0d35fd58c6991d60
dmarinsa <dmarinsanchez@gmail.com>
2021-12-12 01:06:57 +0100

    extract nodes to constants

---
+++ index.js
@@ -0,0 +1,63 @@
+const assert = require("assert");
+
+const Node = (operator, value, left, right) => {
+  const result = function () {
+    switch (this.operator) {
+      case "+":
+        return left.result() + right.result();
+      case "-":
+        return left.result() - right.result();
+      case "x":
+        return left.result() * right.result();
+      case "÷":
+        return left.result() / right.result();
+      default:
+        return value;
+    }
+  };
+
+  const toString = function () {
+    switch (this.operator) {
+      case "+":
+        return `(${left.toString()} + ${right.toString()})`;
+      case "-":
+        return `(${left.toString()} - ${right.toString()})`;
+      case "x":
+        return `(${left.toString()} x ${right.toString()})`;
+      case "÷":
+        return `(${left.toString()} ÷ ${right.toString()})`;
+      default:
+        return value.toString();
+    }
+  };
+
+  return {
+    operator,
+    value,
+    left,
+    right,
+    result,
+    toString
+  };
+};
+
+const number5Node = Node("", 5, null, null);
+const number2Node = Node("", 2, null, null);
+const number3Node = Node("", 3, null, null);
+const number7Node = Node("", 7, null, null);
+const number6Node = Node("", 6, null, null);
+const innerNode = Node("-", null, number3Node, number2Node);
+const multipliedNode = Node("x", null, innerNode, number5Node);
+const add7ToMultipliedNode = Node("+", null, number7Node, multipliedNode)
+
+const tree = Node(
+  "÷",
+  null,
+  add7ToMultipliedNode,
+  number6Node
+);
+
+module.exports = {
+  tree,
+  Node,
+}
